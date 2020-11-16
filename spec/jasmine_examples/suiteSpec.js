379c6472ec24568c2f3505a570b2d9ec8096d6d5
caferion <caferion@ex-em.com>
2020-11-16 22:21:14 +0900

    자스민 테스트 실습

--- spec/jasmine_examples/suiteSpec.js
+++ spec/jasmine_examples/suiteSpec.js
@@ -108,3 +108,291 @@ xdescribe('A spec', () => {
     expect(foo).toEqual(1);
   });
 });
+
+describe('Pending specs', () => {
+  xit('can be declared "xit"', () => {
+    expect(true).toBe(false);
+  });
+  it('can be declared with "it" but without a function');
+
+  it('can be declared by calling "pending" in the spec body', () => {
+    expect(true).toBe(false);
+    pending('this is why it is pending');
+  });
+});
+
+describe('A spy', () => {
+  let foo,
+    bar = null;
+  beforeEach(() => {
+    foo = {
+      setBar(value) {
+        bar = value;
+      },
+    };
+
+    spyOn(foo, 'setBar');
+
+    foo.setBar(123);
+    foo.setBar(456, 'another param');
+  });
+
+  it('tracks that the spy was called', () => {
+    expect(foo.setBar).toHaveBeenCalled();
+  });
+
+  it('tracks that the spy was called x times', () => {
+    expect(foo.setBar).toHaveBeenCalledTimes(2);
+  });
+
+  it('tracks all the arguments of its calls', () => {
+    expect(foo.setBar).toHaveBeenCalledWith(123);
+    expect(foo.setBar).toHaveBeenCalledWith(456, 'another param');
+  });
+
+  it('stops all execution on a function', () => {
+    expect(bar).toBeNull();
+  });
+
+  it('tracks if it was called at all', () => {
+    foo.setBar();
+
+    expect(foo.setBar.calls.any()).toEqual(true);
+  });
+});
+
+describe('A spy, when crated manually', () => {
+  let whatAmI;
+
+  beforeEach(() => {
+    whatAmI = jasmine.createSpy('whatAmI');
+
+    whatAmI('I', 'am', 'a', 'spy');
+  });
+
+  it('tracks that the spy was called', () => {
+    expect(whatAmI).toHaveBeenCalled();
+  });
+});
+
+describe('Multiple spies, when created manually', () => {
+  let tape;
+
+  beforeEach(() => {
+    tape = jasmine.createSpyObj('tape', ['play', 'pause', 'stop', 'rewind']);
+
+    tape.play();
+    tape.pause();
+    tape.rewind(0);
+  });
+
+  it('creates spies for each requested function', () => {
+    expect(tape.play).toBeDefined();
+    expect(tape.pause).toBeDefined();
+    expect(tape.stop).toBeDefined();
+    expect(tape.rewind).toBeDefined();
+  });
+});
+
+describe('Matching with finesse', () => {
+  describe('jasmine.any', () => {
+    it('matches any value', () => {
+      expect({}).toEqual(jasmine.any(Object));
+      expect(12).toEqual(jasmine.any(Number));
+    });
+  });
+  describe('when used with a spy', () => {
+    it('is useful for comparing arguments', () => {
+      const foo = jasmine.createSpy('foo');
+      foo(12, () => true);
+
+      expect(foo).toHaveBeenCalledWith(
+        jasmine.any(Number),
+        jasmine.any(Function)
+      );
+    });
+  });
+
+  describe('jasmine.anything', () => {
+    it('mathces anything', () => {
+      expect(1).toEqual(jasmine.anything());
+    });
+    describe('when used with a spy', () => {
+      it('is useful when the argument can be ignored', () => {
+        let foo = jasmine.createSpy('foo');
+        foo(12, () => false);
+        expect(foo).toHaveBeenCalledWith(12, jasmine.anything());
+      });
+    });
+  });
+
+  describe('jasmine.objectContainig', () => {
+    let foo;
+    beforeEach(() => {
+      foo = {
+        a: 1,
+        b: 2,
+        bar: 'baz',
+      };
+    });
+    it('matches objects with the expect key/value pairs', () => {
+      expect(foo).toEqual(
+        jasmine.objectContaining({
+          bar: 'baz',
+        })
+      );
+      expect(foo).not.toEqual(
+        jasmine.objectContaining({
+          c: 37,
+        })
+      );
+    });
+
+    describe('when used with spy', () => {
+      it('is useful for comparing arguments', () => {
+        const callback = jasmine.createSpy('callback');
+        callback({
+          bar: 'baz',
+        });
+        expect(callback).toHaveBeenCalledWith(
+          jasmine.objectContaining({ bar: 'baz' })
+        );
+      });
+    });
+  });
+
+  describe('jasmine.arrayContaining', () => {
+    let foo;
+    beforeEach(() => {
+      foo = [1, 2, 3, 4];
+    });
+
+    it('matches arrays with some of the values', () => {
+      expect(foo).toEqual(jasmine.arrayContaining([3, 1]));
+      expect(foo).not.toEqual(jasmine.arrayContaining([6]));
+    });
+
+    describe('when used with a spy', () => {
+      it('is useful when comparing arguments', () => {
+        const callback = jasmine.createSpy('callback');
+        callback([1, 2, 3, 4]);
+
+        expect(callback).toHaveBeenCalledWith(
+          jasmine.arrayContaining([4, 2, 3])
+        );
+        expect(callback).not.toHaveBeenCalledWith(
+          jasmine.arrayContaining([5, 2])
+        );
+      });
+    });
+  });
+
+  describe('jasmine.stringMatching', () => {
+    it('matches as a regexp', () => {
+      expect({ foo: 'bar' }).toEqual({ foo: jasmine.stringMatching(/^bar$/) });
+      expect({ foo: 'foobarbaz' }).toEqual({
+        foo: jasmine.stringMatching('bar'),
+      });
+    });
+
+    describe('when used with a spy', () => {
+      it('is useful for comparing arguments', () => {
+        const callback = jasmine.createSpy('callback');
+        callback('foobarbaz');
+
+        expect(callback).toHaveBeenCalledWith(jasmine.stringMatching('bar'));
+        expect(callback).not.toHaveBeenCalledWith(
+          jasmine.stringMatching(/^bar$/)
+        );
+      });
+    });
+  });
+
+  describe('custom asymmetry', () => {
+    const tester = {
+      asymmetricMatch: function (actual) {
+        const secondValue = actual.splict(',')[1];
+        return secondValue === 'bar';
+      },
+    };
+
+    it('dives in deep', () => {
+      expect('foo,bar,baz,quux').toEqual(tester);
+    });
+
+    describe('when used with a spy', () => {
+      it('is useful for comparing arguments', () => {
+        const callback = jasmine.createSpy('callback');
+        callback('foo,bar,baz');
+        expect(callback).toHaveBeenCalledWith(tester);
+      });
+    });
+  });
+
+  describe('Manually ticking the JasmineClock', () => {
+    let timerCallback;
+
+    beforeEach(() => {
+      timerCallback = jasmine.createSpy('timerCallback');
+      jasmine.clock().install();
+    });
+
+    afterEach(() => {
+      jasmine.clock().uninstall();
+    });
+
+    it('causes a timeout to be called synchronuously', () => {
+      setTimeout(() => {
+        timerCallback();
+      }, 100);
+      expect(timerCallback).not.toHaveBeenCalled();
+      jasmine.clock().tick(101);
+      expect(timerCallback).toHaveBeenCalled();
+    });
+
+    it('causes an interval to be called synchronously', () => {
+      setInterval(() => {
+        timerCallback();
+      }, 100);
+      expect(timerCallback).not.toHaveBeenCalled();
+
+      jasmine.clock().tick(101);
+      expect(timerCallback.calls.count()).toEqual(1);
+
+      jasmine.clock().tick(50);
+      expect(timerCallback.calls.count()).toEqual(1);
+
+      jasmine.clock().tick(50);
+      expect(timerCallback.calls.count()).toEqual(2);
+    });
+
+    describe('Mocking the Date object', () => {
+      it('mocks the Date object and sets it to a given time', () => {
+        const baseTime = new Date(2013, 9, 23);
+        jasmine.clock().mockDate(baseTime);
+
+        jasmine.clock().tick(50);
+        expect(new Date().getTime()).toEqual(baseTime.getTime() + 50);
+      });
+    });
+  });
+
+  describe('Asynchronous specs', () => {
+    let value;
+
+    describe('Using callbacks', () => {
+      beforeEach((done) => {
+        setTimeout(() => {
+          value = 0;
+          done();
+        }, 1);
+      });
+
+      it('should support async execution of test preparation and expectattions', (done) => {
+        value++;
+        expect(value).toBeGreaterThan(0);
+        done();
+      });
+    });
+  });
+});
